f05e2b9ffcd9cdd948e204570f8cea5c17584753
diellmustafa <diell200908@gmail.com>
2025-12-17 19:19:21 +0100

    28

--- challenge3/models/recipe.py
+++ challenge3/models/recipe.py
@@ -3,12 +3,12 @@ from typing import Optional
 
 class RecipeBase(BaseModel):
     name: str
-    description: Optional[str]
+    description: Optional[str] = None
     ingredients: str
     instructions: str
     cuisine: str
     difficulty: str
-    category_id: Optional[int]
+    category_id: Optional[int] = None
 
 class RecipeCreate(RecipeBase):
     pass
