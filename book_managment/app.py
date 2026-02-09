1b44c6093d51babcc8b77ad6e2b67900ff64f476
diellmustafa <diell200908@gmail.com>
2026-02-09 19:20:12 +0100

    33.6

--- book_managment/app.py
+++ book_managment/app.py
@@ -154,3 +154,125 @@ if action == 'Update Book':
         new_book_genres = st.text_input('Genres (comma separated)', value=book['genres'])
         new_book_year = st.number_input('Publication Year', min_value=1440, max_value=datetime.now().year,step=1,value=book['publication_year'])
         book_id = book['id']
+
+        if st.button("Update Book"):
+            genres_list = [g.strip() for g in new_book_genres.split(',') if g.strip()]
+            book_data = {
+                "title": new_book_title,
+                "author_id": next((author['id'] for author in authors if author['name'] == selected_author_name), None),
+                "rating": book.get('book_link', ""),
+                "genres": genres_list,
+                "average_rating": new_book_average,
+                "published_year": new_book_year
+            }
+            update_book(api_key, book_id, book_data)
+
+elif action == "Delete Book":
+    book_to_delete = st.selectbox("Select Book to Delete", options=[book['title'] for book in books], key="select_book_delete")
+
+    if st.button("Delete Book"):
+        book_id = next((book['id'] for book in books if book['title'] == book_to_delete), None)
+        delete_book(api_key, book_id)
+
+
+#Creating Visualization
+def visualization_dashboard():
+    st.title("Visualization Dashboard")
+
+    book = get_books()
+    authors = get_authors()
+
+    df_books = pd.DataFrame(books)
+
+    if 'author_id' in df_books.columns:
+        author_id_to_name = {author['id']: author['name'] for author in authors}
+        df_books['author'] = df_books['author_id'].map(author_id_to_name)
+        df_books.drop('author_id', axis=1, inPlace=True)
+
+    st.sidebar.title("Filters")
+
+    selected_author = st.sidebar.selectbox("Select Author", options=["All"] + list(author_id_to_name.values()))
+
+    min_year = int(df_books['published_year'].min())
+    max_year = int(df_books['published_year'].max())
+    selected_year = st.sidebar.slider("Select Published Year", min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
+    selected_rating = st.sidebar.slider("Select Average Rating", min_value=0.0, max_value=5.0, value=(0.0, 5.0), step=0.1)
+
+    filters_applied = selected_author !="All" or selected_year !=(min_year, max_year) or selected_rating != (0.0, 5.0)
+
+    if st.sidebar.button("Apply Filters") or not filters_applied:
+        filters_books = df_books.copy()
+
+        if filters_applied:
+            if selected_author != "All":
+                filters_books = filters_books[filters_books['author'] == selected_author]
+
+                filters_books = filters_books[(filters_books['published_year'] >= selected_year[0]) & (
+                filters_books['published_year'] <= [1])]
+
+                filters_books = filters_books[(filters_books['average_rating'] >= selected_rating[0]) & (
+                filters_books['average_rating'] <= [1])]
+
+
+        if not filters_books.empty:
+            st.subheader(f"Books by Year")
+            books_by_year = filters_books.groupby('published_year').size().reset_index(name='Count')
+            fig_years = px.bar(
+                books_by_year,
+                x = 'published_year',
+                y = 'Count',
+                title=f'Number of Books by Year',
+                labels={"published_year": "Published Year", "Count": "Number of Books"},
+                text='Count'
+            )
+            fig_years.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+            fig_years.update_layout(
+                informtext_minsize = 8,
+                uniformtext_mode = 'hide',
+                xaxis = dict(
+                    tickmode='linear',
+                    tick0=min_year,
+                    dtick=5,
+                    tickangle=-45,
+                    tickfront=dict(size=10)
+                ),
+                yaxis = dict(title='Number of Books', range=[0, books_by_year['Count'].max() + 1]),
+                title_x = -0.5
+            )
+            st.plotly_chart(fig_years, use_container_width=True)
+
+            st.subheader(f"Books by Average Rating")
+            books_by_rating = filters_books.groupby('average_rating').size().rest_index(name='Count')
+            fig_ratings = px.bar(
+                books_by_rating,
+                x = 'average_rating',
+                y = 'Count',
+                title = "Number of Books by Average Rating",
+                labels = {"average_rating": "Average Rating", "Count": "Number of Books"},
+                text='Count'
+            )
+            fig_ratings.update_traces()
+            fig_ratings.update_layout(
+                informtext_minsize=8,
+                uniformtext_mode='hide',
+                yaxis=dict(title='Number of Books', range=[0, books_by_rating['Count'].max() + 1]),
+                title_x=0.5
+            )
+            st.plotly_chart(fig_ratings, use_container_width=True)
+        else:
+            st.warning("No book data available for the selected filters")
+    else:
+        st.warning("No book data available for visualization")
+
+st.sidebar.title("Navigation")
+option = st.sidebar.selectbox("Choose a dashboard", ["Authors Dashboard", "Books Dashboard", "Visualization"])
+if option == "Visualization":
+    visualization_dashboard()
+if api_key_input and validate_api_key(api_key_input):
+    if option == "Authors Dashboard":
+        dashboard_author(api_key_input)
+    if option == "Books Dashboard":
+        books_dashboard(api_key_input)
+else:
+    st.error("Invalid API Key or API Key is missing")
